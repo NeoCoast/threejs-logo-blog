346b8a162be1d9f5cf6789dd6c6595f55e94f46f
FrissA <alexis.friss@gmail.com>
2025-01-16 16:51:40 -0300

    Rotating cube

--- src/NeocoastLogo.jsx
+++ src/NeocoastLogo.jsx
@@ -1,7 +1,18 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
 const NeocoastLogo = ({ ambientLightIntensity }) => {
+  const meshRef = useRef(null);
+
+  useFrame(() => {
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.y += 0.01;
+    meshRef.current.rotation.z += 0.01;
+  });
+
   return (
     <>
-      <mesh>
+      <mesh ref={meshRef}>
         <boxGeometry />
         <meshStandardMaterial color="blue" />
       </mesh>
