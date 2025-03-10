c8bb1709d214c0323b6f516fad6992b3ea9dbdf3
FrissA <alexis.friss@gmail.com>
2025-03-10 14:16:55 -0300

    last commit on JS

--- src/NeocoastLogo.jsx
+++ src/NeocoastLogo.jsx
@@ -1,27 +1,25 @@
-import { useRef } from "react";
-import { useFrame, useLoader } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
 import { SVGLoader } from "three/examples/jsm/Addons.js";
 
 import neocoastLogo from "./assets/neocoastLogo.svg";
+import { OrbitControls } from "@react-three/drei";
 
 const NeocoastLogo = ({ ambientLightIntensity }) => {
-  const meshRef = useRef(null);
 
   const { paths } = useLoader(SVGLoader, neocoastLogo);
 
-  useFrame(() => {
-    meshRef.current.rotation.x += 0.01;
-    meshRef.current.rotation.y += 0.01;
-    meshRef.current.rotation.z += 0.01;
-  });
-
   return (
     <>
-      <mesh ref={meshRef}>
+      <OrbitControls
+        target={[1 - Math.PI, 1, 0]}
+        autoRotate
+        autoRotateSpeed={20}
+      />
+      <mesh>
         <group
           scale={[0.01, 0.01, 0.01]}
           rotation={[0, Math.PI, 0]}
-          position={[2, -1, -1]}
+          position={[0, 0, 0]}
         >
           {paths.map((path, i) =>
             path.toShapes().map((shape, j) => (
@@ -36,6 +34,7 @@ const NeocoastLogo = ({ ambientLightIntensity }) => {
 
       <ambientLight intensity={ambientLightIntensity} />
       <directionalLight color="white" position={[0, 0, 5]} />
+      <directionalLight color="white" position={[0, 0, -5]} />
     </>
   );
 };
